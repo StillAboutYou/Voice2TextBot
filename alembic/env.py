fffe6ae665d89e0ac616f2c810e3d03c3dd367a0
Zyablik <StillAboutYou195@gmail.com>
2024-12-23 01:14:50 +0300

    comments spaces count fix

--- alembic/env.py
+++ alembic/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context # type: ignore
+from alembic import context  # type: ignore
 
 from core.models import Base
 
